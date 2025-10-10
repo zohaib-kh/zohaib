913f6ce5b737c093fe45cf95acf51281b43d15f1
unknown <zk86443@gmail.com>
2025-10-10 04:29:20 -0700

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,31 @@ import { BrowserRouter as Router } from "react-router-dom"; // optional: keeps r
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
  import About from "./pages/about";
+import Home from "./pages/home";
+import Services from "./pages/services";
+import Projects from "./pages/projects";
+import Skills from "./pages/skills";
+import Experience from "./pages/experience";
+import Contact from "./pages/contact";
 
 function App() {
   return (
-    // <Router>
-    //   <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors">
-    //     <Navbar />
-    //     <main className="flex-1">
-    //       <Home />
-    //       <About />
-    //       <Services />
-    //       <Projects />
-    //       <Skills />
-    //       <Experience />
-    //       <Contact />
-    //     </main>
-    //     <Footer />
-    //   </div>
-    // </Router>
-   <About  />
+    <Router>
+      <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors">
+        <Navbar />
+        <main className="flex-1">
+          <Home />
+          <About />
+          <Services />
+          <Projects />
+          <Skills />
+          <Experience />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  
 
     
       
@@ -29,6 +35,4 @@ function App() {
 }
 
 export default App;
-
-// test commit 
  
